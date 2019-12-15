85fda741645861165ea399734ef595d546eb6d11
Andrey Lukashov <andrey.lukashov@gmail.com>
2019-12-14 16:44:01 -0800

    Can accept current time argument and can read STDIN

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ For example: ./application.py 16:10 < config
 - test that there are only 3 arguments
 - test that config is a file and can be opened
 - test that the time supported is in a correct format (regex?)
+- throw exceptions whenever any tests above fail
---
+++ application.py
@@ -0,0 +1,14 @@
+import sys
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("current_time", nargs=1)
+    args = parser.parse_args()
+
+    print("Current time:" + str(args.current_time))
+
+    for line in sys.stdin:
+        print(line)
+
